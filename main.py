63c9a7f21b8f144ccb948ddd975588c48b532c6b
Przemysław Skrzypek <przemchuck.biznes2@gmail.com>
2023-11-25 00:05:35 +0100

    somewhat working

--- main.py
+++ main.py
@@ -12,14 +12,13 @@ recording_finished = threading.Event()
 
 
 while True:
-    print(Fore.GREEN + 'You Pressed A Key!' + Style.RESET_ALL)
     with sr.Microphone() as source:
-        print(Fore.YELLOW + "Speak Anything :" + Style.RESET_ALL)
+        # print(Fore.YELLOW + "Speak Anything :" + Style.RESET_ALL)
         try:
             start_time = time.time()
-            audio_text = r.listen(source)
+            audio_text = r.listen(source, timeout=0.5)
             end_time = time.time()
-            print(Fore.RED + "Stop." + Style.RESET_ALL)
+            # print(Fore.RED + "Stop." + Style.RESET_ALL)
             print(Fore.CYAN + "Powiedziałeś: " + r.recognize_google(audio_text, language='pl-PL') + Style.RESET_ALL)
             duration = end_time - start_time - 1
             print(Fore.MAGENTA + "Time: " + str(duration) + Style.RESET_ALL)
@@ -28,6 +27,9 @@ while True:
                 print(Fore.BLUE + "You said: " + r.recognize_google(audio_text, language='en-US') + Style.RESET_ALL)
                 print(Fore.MAGENTA + "Time: " + str(duration) + Style.RESET_ALL)
             except:
-                print(Fore.RED + "Google Speech Recognition could not understand audio" + Style.RESET_ALL)
+                # print(Fore.RED + "Google Speech Recognition could not understand audio" + Style.RESET_ALL)
+                pass
+        except sr.WaitTimeoutError:
+            pass
         except Exception as e:
             print(Fore.RED + "An error occurred: {0}".format(e) + Style.RESET_ALL)
