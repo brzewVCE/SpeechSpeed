4a79378864cf7ec9434cdc1a7954873485410c77
Przemysław Skrzypek <przemchuck.biznes2@gmail.com>
2023-11-25 00:59:01 +0100

    voice subtitles somewhat working

--- main.py
+++ main.py
@@ -1,31 +1,22 @@
 import speech_recognition as sr
 import time
-import keyboard
 from colorama import Fore, Style
-import threading
-
-# Initialize recognizer class (for recognizing the speech)
+import multiprocessing
+mp_context = multiprocessing.get_context('spawn')
 r = sr.Recognizer()
 
-# Create an Event object
-recording_finished = threading.Event()
-
-
-while True:
+def process_speech():
     with sr.Microphone() as source:
         # print(Fore.YELLOW + "Speak Anything :" + Style.RESET_ALL)
         try:
-            start_time = time.time()
-            audio_text = r.listen(source, timeout=0.5)
-            end_time = time.time()
+            audio_text = r.listen(source, timeout=0.5, phrase_time_limit=3)
+            process = mp_context.Process(target=process_speech)
+            process.start()
             # print(Fore.RED + "Stop." + Style.RESET_ALL)
             print(Fore.CYAN + "Powiedziałeś: " + r.recognize_google(audio_text, language='pl-PL') + Style.RESET_ALL)
-            duration = end_time - start_time - 1
-            print(Fore.MAGENTA + "Time: " + str(duration) + Style.RESET_ALL)
         except sr.UnknownValueError:
             try:
                 print(Fore.BLUE + "You said: " + r.recognize_google(audio_text, language='en-US') + Style.RESET_ALL)
-                print(Fore.MAGENTA + "Time: " + str(duration) + Style.RESET_ALL)
             except:
                 # print(Fore.RED + "Google Speech Recognition could not understand audio" + Style.RESET_ALL)
                 pass
@@ -33,3 +24,13 @@ while True:
             pass
         except Exception as e:
             print(Fore.RED + "An error occurred: {0}".format(e) + Style.RESET_ALL)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    while True:
+        process_speech()
