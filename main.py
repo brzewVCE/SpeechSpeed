4773469f8c384d870925a7c45b7cfb32a4184905
Przemysław Skrzypek <przemchuck.biznes2@gmail.com>
2023-11-25 01:40:32 +0100

    some fixes

--- main.py
+++ main.py
@@ -4,16 +4,20 @@ from colorama import Fore, Style
 import multiprocessing
 mp_context = multiprocessing.get_context('spawn')
 r = sr.Recognizer()
+r.energy_threshold = 0
+r.dynamic_energy_threshold = False
 
 def process_speech():
+    time.sleep(4)
     words = {}
     with sr.Microphone() as source:
         try:
             #Jakoś mądrzej te 3 sekundy zrobić
             #Czasem dwa razy ten sam tekst (?)
-            audio_text = r.listen(source, phrase_time_limit=3)
+            #Niech dodaje nowe słoa, i resetuje się po ciszy
             process = mp_context.Process(target=process_speech)
             process.start()
+            audio_text = r.listen(source, phrase_time_limit=3)
             words = r.recognize_google(audio_text, language='pl-PL').split()
             print(Fore.CYAN + "Powiedziałeś: " + r.recognize_google(audio_text, language='pl-PL') + Style.RESET_ALL)
         except sr.UnknownValueError:
