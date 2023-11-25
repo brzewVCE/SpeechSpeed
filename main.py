7d9574a6a77e5411b184f1ae212341413d9460c7
Przemysław Skrzypek <przemchuck.biznes2@gmail.com>
2023-11-25 01:33:31 +0100

    initial commit

--- main.py
+++ main.py
@@ -6,25 +6,29 @@ mp_context = multiprocessing.get_context('spawn')
 r = sr.Recognizer()
 
 def process_speech():
+    words = {}
     with sr.Microphone() as source:
-        # print(Fore.YELLOW + "Speak Anything :" + Style.RESET_ALL)
         try:
-            audio_text = r.listen(source, timeout=0.5, phrase_time_limit=3)
+            #Jakoś mądrzej te 3 sekundy zrobić
+            #Czasem dwa razy ten sam tekst (?)
+            audio_text = r.listen(source, phrase_time_limit=3)
             process = mp_context.Process(target=process_speech)
             process.start()
-            # print(Fore.RED + "Stop." + Style.RESET_ALL)
+            words = r.recognize_google(audio_text, language='pl-PL').split()
             print(Fore.CYAN + "Powiedziałeś: " + r.recognize_google(audio_text, language='pl-PL') + Style.RESET_ALL)
         except sr.UnknownValueError:
             try:
+                words = r.recognize_google(audio_text, language='en-US').split()
                 print(Fore.BLUE + "You said: " + r.recognize_google(audio_text, language='en-US') + Style.RESET_ALL)
             except:
-                # print(Fore.RED + "Google Speech Recognition could not understand audio" + Style.RESET_ALL)
                 pass
         except sr.WaitTimeoutError:
             pass
         except Exception as e:
             print(Fore.RED + "An error occurred: {0}".format(e) + Style.RESET_ALL)
-
+    # if len(words) != 0:
+    #     print(len(words))
+    print(words)
 
 
 
