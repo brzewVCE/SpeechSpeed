141cdefc74967932c26bd1718158e02a280b389d
Przemysław Skrzypek <przemchuck.biznes2@gmail.com>
2023-11-28 20:36:07 +0100

    fixed loop

--- main.py
+++ main.py
@@ -2,37 +2,48 @@ import speech_recognition as sr
 import time
 from colorama import Fore, Style
 import multiprocessing
+import argparse
 mp_context = multiprocessing.get_context('spawn')
 r = sr.Recognizer()
 r.energy_threshold = 0
 r.dynamic_energy_threshold = False
 
-def process_speech():
-    time.sleep(4)
+
+def print_col(text, color):
+    color_constant = getattr(Fore, color.upper(), None)
+    if color_constant:
+        print(color_constant + text + Style.RESET_ALL)
+    else:
+        print(text)
+
+def process_speech(process_num, output_mode=False):
+    if output_mode:
+        print_col(f"Started process_speech() function number {process_num}", "red")
+    time.sleep(3)
     words = {}
     with sr.Microphone() as source:
         try:
             #Jakoś mądrzej te 3 sekundy zrobić
             #Czasem dwa razy ten sam tekst (?)
             #Niech dodaje nowe słoa, i resetuje się po ciszy
-            process = mp_context.Process(target=process_speech)
+            process = mp_context.Process(target=process_speech, args=(process_num+1,output_mode,))
             process.start()
             audio_text = r.listen(source, phrase_time_limit=3)
             words = r.recognize_google(audio_text, language='pl-PL').split()
-            print(Fore.CYAN + "Powiedziałeś: " + r.recognize_google(audio_text, language='pl-PL') + Style.RESET_ALL)
         except sr.UnknownValueError:
             try:
                 words = r.recognize_google(audio_text, language='en-US').split()
-                print(Fore.BLUE + "You said: " + r.recognize_google(audio_text, language='en-US') + Style.RESET_ALL)
             except:
                 pass
         except sr.WaitTimeoutError:
             pass
         except Exception as e:
             print(Fore.RED + "An error occurred: {0}".format(e) + Style.RESET_ALL)
-    # if len(words) != 0:
-    #     print(len(words))
-    print(words)
+    if len(words) != 0 and output_mode:
+        print_col(f"Process {process_num} recognized: {words}", "green")
+        print(len(words))
+    return print_col(f"Słowa na minutę: {len(words)/3*60}", "green")
+    
 
 
 
@@ -40,5 +51,8 @@ def process_speech():
 
 
 if __name__ == '__main__':
-    while True:
-        process_speech()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--output', action='store_true', help='Enable output mode')
+    args = parser.parse_args()
+
+    process_speech(0,args.output)
