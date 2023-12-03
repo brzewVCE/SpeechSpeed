5453accc5e670b04cf209e99ec761a45e3e6b39f
Przemysław Skrzypek <przemchuck.biznes2@gmail.com>
2023-12-03 16:19:48 +0100

    My apolocheese, forgot to check into beta. Some GUI changes

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ r.energy_threshold = 0
 r.dynamic_energy_threshold = False
 
 ctk.set_appearance_mode("system")
-ctk.set_default_color_theme("light")
 
-root = ctk.Tk()
+root = ctk.CTk()
 root.geometry("500x350")
 
 rec_time=4
 offset=2
 previous_sentence = []
+WPM = 0
 
 def print_col(text, color):
     color_constant = getattr(Fore, color.upper(), None)
@@ -34,7 +34,7 @@ def print_wpm(words):
     sys.stdout.flush()
 
 #zdesignować GUI
-def process_speech(process_num, output_mode=False, rec_time=rec_time, offset=offset, previous_sentence=previous_sentence):
+def process_speech(process_num, output_mode=False, rec_time=rec_time, offset=offset, previous_sentence=previous_sentence, WPM=WPM):
     time.sleep(rec_time-offset)
     print_col(f"Started process_speech() function number {process_num}", "red")
     words = {}
@@ -66,13 +66,13 @@ def process_speech(process_num, output_mode=False, rec_time=rec_time, offset=off
             words = words[1:]
     
     previous_sentence = words
+    WPM = len(words) / rec_time * 60
     print_col(f"Process {process_num} recognized: {words}", "green")
-    print_col(f"Words per minute: {len(words) / rec_time * 60}", "green")
-    return previous_sentence
+    print_col(f"Words per minute: {WPM}", "green")
+
+    return previous_sentence, WPM
     
     
-
-
 
 
 
@@ -84,3 +84,13 @@ if __name__ == '__main__':
 
     process = mp_context.Process(target=process_speech, args=(0,args.output,rec_time,rec_time))
     process.start()
+    frame = ctk.CTkFrame(root)
+    frame.pack(pady=20, padx=20, fill="both", expand=True)
+
+    label = ctk.CTkLabel(frame, text="Words Per Minute:")
+    label.pack(padx=10, pady=10)
+
+    label = ctk.CTkLabel(frame, text=WPM)
+    label.pack(padx=10, pady=10)
+
+    # root.mainloop()
